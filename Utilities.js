3afa0b76af13082115c64cf530d8667b65c2f55c
Matias Caceres <matiascaceres.3197@gmail.com>
2025-08-09 07:05:15 -0300

    Cambios varios

--- Utilities.js
+++ Utilities.js
@@ -1,14 +1,14 @@
 // ------------------------ Datos ------------------------
 const especializaciones = [
-  { nombre: "Espada", profesion: "Guerrero", descripción: "Al elegir la espada como tu arma seguiras el camino del Caballero. Disiplinados y valerosos gerreros.", precio: 0 },
-  { nombre: "Hacha", profesion: "Guerrero", descripción: "Al elegir la hacha como tu arma seguiras el camino del Bárbaro. Salvajes y feroces gerreros.", precio: 0 },
-  { nombre: "Maza", profesion: "Guerrero", descripción: "Al elegir la maza como tu arma seguiras el camino del Templario. Resistentes e incorruptibles gerreros.", precio: 0 },
-  { nombre: "Libro de mágia elemental", profesion: "Mago", descripción: "El Libro de mágia elemental te llevará por el camino de los Hechiceros. Poderosos magos capaces de manipular los elementos y la naturaleza.", precio: 0 },
-  { nombre: "Báculo de invocación", profesion: "Mago", descripción: "El Báculo de invocación te llevará por le camino del los Invocadores. Poderosos magos capaces de invocar criaturas. Ya sean, o no, de este mundo.", precio: 0 },
-  { nombre: "Amuleto de Druida", profesion: "Mago", descripción: "El Amuleto de Druida te llevará por le camino del los Druidas. Poderosos magos capaces de sanar cualquier Herida y con el don de la clarividencia.", precio: 0 },
-  { nombre: "Arco", profesion: "Picaro", descripción: "Dominar el Arco te permitirá eliminar a tus enemigos antes de que ellos puedan tocarte.", precio: 0 },
-  { nombre: "Dagas", profesion: "Picaro", descripción: "Dominar las Dagas te permitirá eliminar a tus enemigos antes de que estos noten tu presencia. Las sombras serán tus mejores aliadas.", precio: 0 },
-  { nombre: "Bastón de monje", profesion: "Picaro", descripción: "Dominar Bastón de monje requiere de una impecable agilidad y autocontrol. Tu mente y cuerpo deben ser uno.", precio: 0 }
+  { nombre: "Espada", profesion: "Guerrero", descripción: "Al elegir la espada como tu arma seguiras el camino del Caballero. Disiplinados y valerosos gerreros.", creditosRequeridos: 25 },
+  { nombre: "Hacha", profesion: "Guerrero", descripción: "Al elegir la hacha como tu arma seguiras el camino del Bárbaro. Salvajes y feroces gerreros.", creditosRequeridos: 50 },
+  { nombre: "Maza", profesion: "Guerrero", descripción: "Al elegir la maza como tu arma seguiras el camino del Templario. Resistentes e incorruptibles gerreros.", creditosRequeridos: 75 },
+  { nombre: "Libro de mágia elemental", profesion: "Mago", descripción: "El Libro de mágia elemental te llevará por el camino de los Hechiceros. Poderosos magos capaces de manipular los elementos y la naturaleza.", creditosRequeridos: 25 },
+  { nombre: "Báculo de invocación", profesion: "Mago", descripción: "El Báculo de invocación te llevará por le camino del los Invocadores. Poderosos magos capaces de invocar criaturas. Ya sean, o no, de este mundo.", creditosRequeridos: 50 },
+  { nombre: "Amuleto de Druida", profesion: "Mago", descripción: "El Amuleto de Druida te llevará por le camino del los Druidas. Poderosos magos capaces de sanar cualquier Herida y con el don de la clarividencia.", creditosRequeridos: 75 },
+  { nombre: "Arco", profesion: "Picaro", descripción: "Dominar el Arco te permitirá eliminar a tus enemigos antes de que ellos puedan tocarte.", creditosRequeridos: 25 },
+  { nombre: "Dagas", profesion: "Picaro", descripción: "Dominar las Dagas te permitirá eliminar a tus enemigos antes de que estos noten tu presencia. Las sombras serán tus mejores aliadas.", creditosRequeridos: 50 },
+  { nombre: "Bastón de monje", profesion: "Picaro", descripción: "Dominar Bastón de monje requiere de una impecable agilidad y autocontrol. Tu mente y cuerpo deben ser uno.", creditosRequeridos: 75 }
 ];
 
 
@@ -49,13 +49,13 @@ function casteoANombrePropio(profesionElecta){
     return profesionElecta; 
 }
 
-function mensajeDeeleccionDeEspecializacion(especializacionesFiltradas, profesionElecta){
+function mensajeDeEleccionDeEspecializacion(especializacionesFiltradas, profesionElecta){
 
-    let mensaje = `Has elegido ser un ${profesionElecta}. Priemro, necesitamos que elijas una especialización: \n 
+    let mensaje = `Has elegido ser un ${profesionElecta}. Como regalo de bienvenida te daremos 100 Créditos para que elijas una especialización: \n 
 
-        1- ${especializacionesFiltradas[0].nombre}.\n ${especializacionesFiltradas[0].descripción}
-        2- ${especializacionesFiltradas[1].nombre}.\n ${especializacionesFiltradas[1].descripción}
-        3- ${especializacionesFiltradas[2].nombre}.\n ${especializacionesFiltradas[2].descripción}`
+        1- ${especializacionesFiltradas[0].nombre}. Costo: ${especializacionesFiltradas[0].creditosRequeridos} Créditos.\n ${especializacionesFiltradas[0].descripción}
+        2- ${especializacionesFiltradas[1].nombre}. Costo: ${especializacionesFiltradas[1].creditosRequeridos} Créditos.\n ${especializacionesFiltradas[1].descripción}
+        3- ${especializacionesFiltradas[2].nombre}. Costo: ${especializacionesFiltradas[2].creditosRequeridos} Créditos.\n ${especializacionesFiltradas[2].descripción}`
 
     let especializacionElecta = parseInt(prompt(mensaje));
 
@@ -67,6 +67,6 @@ export function eleccionDeEspecializacion(profesionElecta){
     profesionElecta = casteoANombrePropio(profesionElecta);
 
     const especializacionesFiltradas = especializaciones.filter(especializacion => especializacion.profesion === profesionElecta);
-    mensajeDeeleccionDeEspecializacion(especializacionesFiltradas, profesionElecta);
+    mensajeDeEleccionDeEspecializacion(especializacionesFiltradas, profesionElecta);
 
 }
